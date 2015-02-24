fa0db49879779e688c8b003c1b452bd2872b39ab
sagardave <davesagar2012@gmail.com>
2015-02-24 14:22:24 -0500

    Problem 4 solution

---
+++ P4.py
@@ -0,0 +1,17 @@
+
+def isPalindrome(num):
+	if(str(num) == str(num)[::-1]):
+		return True;
+	else:
+		return False;
+
+maxPalindrome = 0;
+currentNum = 0;
+for i in range (100,1000):
+	for j in range(100,1000):
+		currentNum = i * j;
+		if(isPalindrome(currentNum)):
+			if(currentNum > maxPalindrome):
+				maxPalindrome = currentNum 
+print maxPalindrome;
+
